1101370adef5e410ae77cceca5da79ef12c4a39d
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-05-12 20:03:28 +0200

    Add files via upload

---
+++ api.ts
@@ -0,0 +1,2 @@
+
+"https://api.ibanapi.com/v1/validate/EE471000001020145685?api_key=API_KEY"
--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
-// import { serve } from "https://deno.land/std/http/server.ts";
-
-// serve((req) => new Response("Hello World\n"));
+// import {countries} from "./countries.json"
 
 function entryCount() {
-  const count = prompt("Count number ...")
-  console.log({ count })
+  const acount = prompt("Count number ...");
+  const country = acount?.slice(0,3)
+  console.log( country );
+  if (acount == null) {
+    return { error: "Please provide a valid count number" };
+  } else if (acount.length > 34) {
+    return {
+      error: "Revise your count number, it s not correct ",
+    };
+  }
+
+  // if (acount[0-1] == countries){
+  //   return countries
+  // } 
 }
 
-entryCount()
+entryCount();
